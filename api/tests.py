e9186e3c0a9909a006c041ceb113e455dab7b62f
bulga <bulgaadev@gmail.com>
2013-01-15 11:04:59 +0800

    R2: Test for api.views.new_msg

--- api/tests.py
+++ api/tests.py
@@ -1,16 +1,32 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
 from django.test import TestCase
+from security.models import Account
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+class APITest(TestCase):
+    def test_new_msj(self):
+        # Check when got wrong message
+        phone = 99999999
+        msg = '(Wrong message)'
+        url = '/api/?phone=%%2B976596&msg=%s'
+
+        response = self.client.get(url % msg)
+
+        self.assertEqual(response.status_code, 302)
+        with self.assertRaises(Account.DoesNotExist):
+            Account.objects.get(phone_number=phone)
+        
+        # Check add new account
+        msg = '(Tand %s dugaaraas 100 negj ilgeelee)' % phone
+        response = self.client.get(url % msg)
+
+        self.assertEqual(response.status_code, 302)
+        acc = Account.objects.get(phone_number=phone)
+        self.assertEqual(acc.credit, 100)
+        
+        # Check credit add
+        response = self.client.get(url % msg)
+
+        self.assertEqual(response.status_code, 302)
+        acc = Account.objects.get(phone_number=phone)
+        self.assertEqual(acc.credit, 200)
+
