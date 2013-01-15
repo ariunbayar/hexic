b29e3edc5e82b661a3092c3021bc722166e7000e
bulga <bulgaadev@gmail.com>
2013-01-15 11:24:32 +0800

    R3: Test for login and logout

--- security/tests.py
+++ security/tests.py
@@ -1,16 +1,42 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
+from security.models import Account
 from django.test import TestCase
+from django.core.urlresolvers import reverse
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+class SecurityTest(TestCase):
+    def setUp(self):
+        acc = Account(
+                phone_number=99887766,
+                pin_code=1234,
+                credit=0)
+        acc.save()
+
+    def test_login(self):
+        # When wrong phone number
+        response = self.client.post(reverse('security.views.login'), {
+                                    'phone_number': 99887755,
+                                    'pin_code': 1234})
+        self.assertEqual(response.status_code, 200)
+        response = self.client.get(reverse('game.views.board'))
+        self.assertEqual(response.status_code, 302)
+
+        # When input is correct
+        response = self.client.post(reverse('security.views.login'), {
+                                    'phone_number': 99887766,
+                                    'pin_code': 1234})
+
+        self.assertEqual(response.status_code, 302)
+        response = self.client.get(reverse('game.views.board'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_logout(self):
+        # Logging in
+        response = self.client.post(reverse('security.views.login'), {
+                                    'phone_number': 99887766,
+                                    'pin_code': 1234})
+
+        #Logging out
+        response = self.client.get(reverse('security.views.logout'))
+
+        response = self.client.get(reverse('game.views.board'))
+        self.assertEqual(response.status_code, 302)
