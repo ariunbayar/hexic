f943bd33814a2a23372de25e4d5e3a361deed2d0
Ariunbayar Baterdene <1480970+ariunbayar@users.noreply.github.com>
2013-03-08 11:33:06 +0800

    G25: Removed unnecessary files and moved some

--- hexenv/admin/urls.py
+++
@@ -1,6 +0,0 @@
-from django.conf.urls.defaults import patterns, url
-
-urlpatterns = patterns('',
-    url(r'^admin/board$', 'admin.views.show_board', name='admin-board'),
-    url(r'^admin/moves$', 'admin.views.show_moves', name='admin-moves'),
-)
