f22ce6abb2f751745ea46d6397b9c35d3cbb0426
bulga <bulgaadev@gmail.com>
2013-03-08 11:39:04 +0800

    G25: Included old urls, removed old hexic

--- urls.py
+++ urls.py
@@ -3,8 +3,9 @@ from django.conf.urls.defaults import patterns, include, url
 
 urlpatterns = patterns('',
     url(r'^$', include('public.urls')),
+    url(r'', include('game_old.urls')),
     url(r'^admin/', include('admin.urls')),
-    url(r'^game/', include('game.urls')),
+    url(r'^game_dev/', include('game.urls')),
     url(r'^security/', include('security.urls')),
     url(r'^api/', include('api.urls'))
 )
