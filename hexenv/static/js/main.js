020b9471842e5065c596c0685040100bd5fe1189
Ariunbayar Baterdene <1480970+ariunbayar@users.noreply.github.com>
2013-03-08 11:14:27 +0800

    G25: Moved static files and runner thread

--- hexenv/static/js/main.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-    $('#language-select a').click(function(){
-        var lang = $(this).attr('lang');
-        $('#language-submit input:[name=language]').val(lang);
-        $('#language-submit').submit();
-        return false;
-    });
-
-});
