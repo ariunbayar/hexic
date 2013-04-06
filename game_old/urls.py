a932643d81e91c01ea65fdf072c425763b5856b7
bulga <bulgaadev@gmail.com>
2013-04-06 19:12:50 +0800

    R32: Нүдэнд хулганаараа нэг товшиход шилжилт зогсоно

--- game_old/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls.defaults import *
-
-urlpatterns = patterns('game_old.views',
-    url(r'^game/$', 'home', name='homepage'),
-    url(r'^game/restart$', 'game_restart', name='game-restart'),
-    url(r'^game/select_cell/$', 'select_cell', name='select_cell'),
-)
